79f36e77e27fde2a7896ab54f95eef5aec967cc6
kaichun <50533292@qq.com>
2017-03-16 15:27:27 +0800

    Update tw.sh

--- tw.sh
+++ tw.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
 # Docker installation
 ## -sSL https://get.docker.com/ | sh
-
 # Install Docker Compose
 ##curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-
 # Grant execute permission
 ##chmod +x /usr/local/bin/docker-compose
-
 # Prepare local storage for mapping
 cd /usr
 mkdir tw
@@ -21,6 +17,5 @@ cd ../asset
 ##wget [asset.zip]
 ##unzip asset.zip
 cd /usr/tw
-
 # Get docker compose and run
 curl -k -L https://raw.githubusercontent.com/kaichun/devopstw/master/Docker-Compose-TW-Enablement.sh | bash
