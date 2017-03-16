7adfdbae335dba0203d6c8956a9af9944944d83a
kaichun <50533292@qq.com>
2017-03-16 16:06:36 +0800

    Update tw.sh

--- tw.sh
+++ tw.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd ~
-mkdir /usr/tw
-cd /usr/tw
-mkdir app
-mkdir asset
-cd app
-cd ../asset
-cd /usr/tw
+mkdir -p /usr/tw/
+cd /usr/tw/
+mkdir app/
+mkdir asset/
+cd app/
+cd ../asset/
+cd /usr/tw/
 # Get docker compose and run
 curl -k -L https://raw.githubusercontent.com/kaichun/devopstw/master/Docker-Compose-TW-Enablement.sh | bash
