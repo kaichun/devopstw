f657c7224e0ec31ebdbae9c74fb06b24a392f09f
kaichun <50533292@qq.com>
2017-03-16 18:31:21 +0800

    Update Docker-Compose-TW-Enablement.sh

--- Docker-Compose-TW-Enablement.sh
+++ Docker-Compose-TW-Enablement.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/kaichun/devopstw/master/docker-compose.yml --no-check-certificate
+#!/bin/sh
+start_docker_compose(){
+wget=/usr/bin/wget
+WORKING_DIR="/usr/tw"
+
+cd $WORKING_DIR
+$wget https://raw.githubusercontent.com/kaichun/devopstw/master/docker-compose.yml --no-check-certificate
 docker-compose up -d
+}
+
+start_docker_compose
