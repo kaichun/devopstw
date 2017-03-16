418ca5862996ec02f8371fd2295a6be8b6fca8b0
kaichun <50533292@qq.com>
2017-03-16 13:42:08 +0800

    Add files via upload

---
+++ tw.sh
@@ -0,0 +1,24 @@
+# Docker installation
+## -sSL https://get.docker.com/ | sh
+
+# Install Docker Compose
+##curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+
+# Grant execute permission
+##chmod +x /usr/local/bin/docker-compose
+
+# Prepare local storage for mapping
+cd /usr
+mkdir tw
+cd tw
+mkdir app
+mkdir asset
+cd app
+##wget [app.war]
+cd ../asset
+##wget [asset.zip]
+##unzip asset.zip
+cd /usr/tw
+
+# Get docker compose and run
+curl -k -L https://raw.githubusercontent.com/kaichun/devopstw/master/Docker-Compose-TW-Enablement.sh | bash
